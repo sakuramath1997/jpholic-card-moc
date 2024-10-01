fd10c5f6c8689525c797c6351d024970d6f622dc
sakuramath1997 <takahashi.1997.jp@gmail.com>
2024-10-01 11:26:31 +0900

    initial project

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "34c6f6d0-6d8c-4577-8d15-c023a452e832");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
